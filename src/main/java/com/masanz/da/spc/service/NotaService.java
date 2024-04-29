63bb70a7485d75a84c7dcff444ed80bb69ec099c
aetxabao <aitor.etxabarren@gmail.com>
2024-04-30 00:45:20 +0200

    Base

---
+++ src/main/java/com/masanz/da/spc/service/NotaService.java
@@ -0,0 +1,43 @@
+package com.masanz.da.spc.service;
+
+import com.masanz.da.spc.dao.INotaDao;
+import com.masanz.da.spc.dto.Nota;
+import com.masanz.da.spc.dao.NotaDaoDb;
+import com.masanz.da.spc.dao.NotaDaoMap;
+
+import java.util.*;
+
+public class NotaService {
+
+    private INotaDao notaDao;
+
+    public NotaService(){
+//        notaDao = new NotaDaoMap();
+        notaDao = new NotaDaoDb();
+    }
+
+    public int obtenerNumeroNotas() {
+        return notaDao.obtenerNumeroNotas();
+    }
+
+    public List<Nota> obtenerNotas(int pagina, int notasPorPagina) {
+        return notaDao.obtenerNotas(pagina, notasPorPagina);
+    }
+
+    public Nota obtenerNota(long idNota) {
+        return notaDao.obtenerNota(idNota);
+    }
+
+    public Nota guardarNota(Nota nota) {
+        return notaDao.guardarNota(nota);
+    }
+
+    public boolean actualizarNota(Nota nota) {
+        return notaDao.actualizarNota(nota);
+    }
+
+    public boolean eliminarNota(int idNota) {
+        return notaDao.eliminarNota(idNota);
+    }
+
+}
